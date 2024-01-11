f665f4b2f8f25b7c3aac38cb5698f8d70ee0bcf0
Jonathan <148167.jw@gmail.com>
2024-01-11 15:25:22 +0100

    Removed microcontroller5.0 files as they were used for testing

--- Core/Inc/top_board/rem.h
+++
@@ -1,9 +0,0 @@
-#ifndef __REM_H
-#define __REM_H
-
-#include "stm32f7xx_hal.h"
-
-void REM_UARTinit(UART_HandleTypeDef *huart);
-void REM_UARTCallback(UART_HandleTypeDef *huart);
-
-#endif /* __REM_H */
