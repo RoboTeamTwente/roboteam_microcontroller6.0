c07d2e0ae0643d3e891154389dd2fb309c36dbaf
Julie <j.muchembled@roboteamtwente.nl>
2024-02-13 14:06:45 +0100

    Checking motor constants bis

--- Core/Inc/general/Util/control_util.h
+++ Core/Inc/general/Util/control_util.h
@@ -23,16 +23,14 @@
 #define sinBack sinf(BACK_ANGLE * M_PI/180)
 
 // Wheels
-///////////// CHECK AGAIN !
 #define PWM_CUTOFF 200.0F 		// arbitrary threshold to avoid motor shutdown /// where does that number come from?
 #define WHEEL_GEAR_RATIO 1.0F 	// gear ratio between motor and wheel
 #define PWM_LIMIT MAX_PWM 		// should be equal to MAX_PWM by default
 #define MAX_VOLTAGE 24.0	// [V] see datasheet // NEW MOTOR: ECXFL32L 48V (we use the 24V version)
 float SPEED_CONSTANT; 	
 #define SPEED_CONSTANT_MOTOR 291.0 //[(rpm/V] see datasheet // translation from rpm/V to rad/s/V is done later.
-#define WHEEL_PULSES_PER_ROTATION (float)4*2048 // number of pulses of the encoder per rotation of the motor. New encoder: ENX 32 MILE // should we delete the multiplication by 4?
+#define WHEEL_PULSES_PER_ROTATION (float)4*2048 // number of pulses of the encoder per rotation of the motor. New encoder: ENX 32 MILE // Multiplication by 4 is unsure but datahseet from Maxon is dfferent than some sources.
 
-/// To check !!!
 float OMEGAtoPWM; // conversion factor from wheel speed [rad/s] to required PWM on the motor
 #define WHEEL_ENCODER_TO_OMEGA (float)2*M_PI/(TIME_DIFF*WHEEL_GEAR_RATIO*WHEEL_PULSES_PER_ROTATION) // conversion factor from number of encoder pulses to wheel speed [rad/s]
 
