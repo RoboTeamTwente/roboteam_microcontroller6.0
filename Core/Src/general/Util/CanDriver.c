375f50390482f265e628a44d53e02feedc07e12c
Jonathan <148167.jw@gmail.com>
2024-03-05 13:01:59 +0100

    memcopy data instead of RxData

--- Core/Src/general/Util/CanDriver.c
+++ Core/Src/general/Util/CanDriver.c
@@ -81,12 +81,12 @@ bool extract_command(uint8_t RxData[], CAN_RxHeaderTypeDef *Header){
     } else if (MailBox_two.empty){
         MailBox_two.empty = false;
         MailBox_two.message_id = message_ID;
-        memcpy(MailBox_two.data_Frame, RxData, sizeof(*RxData));
+        memcpy(MailBox_two.data_Frame, data, sizeof(data)));
         return true;
     } else if (MailBox_three.empty){
         MailBox_three.empty = false;
         MailBox_three.message_id = message_ID;
-        memcpy(MailBox_three.data_Frame, RxData, sizeof(*RxData));
+        memcpy(MailBox_three.data_Frame, data, sizeof(data));
         return true;
     }
 
