be61dcc1861be19530a7eb9908d431113a789a7c
Jonathan <148167.jw@gmail.com>
2024-03-12 15:14:52 +0100

    Initial commit kicker

---
+++ Core/Inc/kicker_chipper_board/kicker_board.h
@@ -0,0 +1,14 @@
+#ifndef _KICKER_BOARD_H_
+#define _KICKER_BOARD_H_
+
+#include "logging.h"
+#include "peripheral_util.h"
+#include "CanDriver.h"
+#include <string.h>
+
+void init();
+void loop();
+uint8_t robot_get_ID();
+uint8_t robot_get_Channel();
+
+#endif /* _KICKER_BOARD_H_ */
