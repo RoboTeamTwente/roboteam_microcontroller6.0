af3c4d84bd936eee6b80e93389efc3e0a4fce717
Jonathan <148167.jw@gmail.com>
2024-05-01 16:11:03 +0200

    re-enable printf-float and scanf-float

--- CompilerFlags.py
+++ CompilerFlags.py
@@ -106,8 +106,7 @@ else:
       opt,
       "-mfloat-abi=hard",
       "-mfpu=fpv4-sp-d16",
-      #"-Wl,-u,_printf_float,-u,_scanf_float"
-      "-Wl,--print-memory-usage",
+      "-Wl,-u,_printf_float,-u,_scanf_float"
     ]
   )
 
