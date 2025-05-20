ea63b7b0e822ebd6817d3582e8c08cd2868de660
MarijnBuitenweg <marijnbuitenweg@gmail.com>
2025-05-20 16:54:46 +0200

    Add automatic discretization script

---
+++ Control/Scripts/discretize_plant.m
@@ -0,0 +1,46 @@
+function discretize_plant(ts, model)
+proj = currentProject;
+
+if nargin < 2
+    model = "Plant";
+end
+
+model_d = model+"_d"
+model_fname = "Subsystems/"+model+".slx";
+model_d_fname = "Subsystems/"+model_d+".slx";
+
+copyfile(model_fname, model_d_fname, 'f');
+addFile(proj, model_d_fname);
+load_system(model_d);
+ref_models = find_system(model_d, 'RegExp','on','BlockType','SubSystem','ReferencedSubsystem', '.');
+sldiscmdl(model_d, ts, {'all', 'parammask', 'current', 'off'});
+
+% Extract unique submodels
+subfiles = [""];
+for i=1:length(ref_models)
+    file = get_param(ref_models(i), 'ReferencedSubSystem');
+    if sum(subfiles == file)==0
+        parts = split(file, '.');
+        subfiles = [subfiles, parts(1)];
+    end
+end
+subfiles = subfiles(2:end);
+
+% Discretize unique submodels
+for i=1:length(subfiles)
+    discretize_plant(ts, subfiles(i));
+end
+
+% Change the referenced submodels to their new discretized versions
+for i=1:length(ref_models)
+    file = get_param(ref_models(i), 'ReferencedSubSystem');
+    parts = split(file, '.');
+    set_param(string(ref_models(i)), "ReferencedSubSystem", parts(1)+"_d");
+end
+
+% Finalize the job
+save_system(model_d);
+close_system(model_d);
+
+end
+
